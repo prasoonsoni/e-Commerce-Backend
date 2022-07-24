8eaf8546dc1f9fafdefab6ce8660026722c27a1a
prasoonsoni <prasoonsoni03@gmail.com>
2022-07-24 23:02:23 +0530

    Initial Setup Done

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('<center><h1>Welcome to E-Commerce Backend</h1>' +
+        '<h3><a href="https://github.com/prasoonsoni/Social-Media-Backend" target="_blank">Visit Repository</a></h3></center>')
+})
+
+app.listen(port, () => {
+    console.log(`E-Commerce Backend listening on http://localhost:${port}`)
+})
