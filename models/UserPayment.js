068251388be997ca9878c5c5842d4d6dbe90d409
prasoonsoni <prasoonsoni03@gmail.com>
2022-07-25 18:44:39 +0530

    Added UserPayment Model

---
+++ models/UserPayment.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const UserPaymentSchema = new Schema({
+    user_id: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+    payment_type: { type: String, required: true },
+    provider: { type: String, required: true },
+    account_number: { type: Number, required: true },
+    expiry_date: { type: Number, required: true },
+    created_at: { type: Number, default: Date.now() },
+    modified_at: { type: Number, default: Date.now() }
+})
+
+module.exports = new mongoose.model('UserPayment', UserPaymentSchema)
