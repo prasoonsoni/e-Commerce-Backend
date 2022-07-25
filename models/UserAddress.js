9e1416a95fe364723aada5d5883b857e51863d47
prasoonsoni <prasoonsoni03@gmail.com>
2022-07-25 18:32:55 +0530

    Added UserAddress Model

---
+++ models/UserAddress.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const UserAddressSchema = new Schema({
+    user_id: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+    arrdess_line_1: { type: String, required: true },
+    address_line_2: { type: String, required: true },
+    city: { type: String, required: true },
+    postal_code: { type: Number, required: true },
+    state: { type: String, required: true },
+    country: { type: String, required: true },
+    telephone: { type: Number, required: true },
+    mobile: { type: Number, required: true },
+    created_at: { type: Number, default: Date.now() },
+    modified_at: { type: Number, default: Date.now() }
+})
+
+module.exports = new mongoose.model('UserAddress', UserAddressSchema)
