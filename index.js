7fdb07688faa2309a4105481fd240348c2f6d4ca
prasoonsoni <prasoonsoni03@gmail.com>
2022-07-26 09:31:34 +0530

    Added Database Connection

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const connectToDatabase = require('./database/connection')
 const app = express()
 const cors = require('cors')
 const port = 3000
-
+connectToDatabase()
 app.use(cors())
 app.use(express.json())
 
