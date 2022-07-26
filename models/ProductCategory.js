4d6a3a10b9f726173374fab3a95ad3cea4b06006
prasoonsoni <prasoonsoni03@gmail.com>
2022-07-26 09:42:04 +0530

    Added Product Category Model

---
+++ models/ProductCategory.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const ProductCategorySchema = new Schema({
+    name: { type: String, required: true },
+    description:{type:String, required:true},
+    created_at: { type: Number, default: Date.now() },
+    modified_at: { type: Number, default: Date.now() },
+    deleted_at:{type:Number}
+})
+
+module.exports = mongoose.model('ProductCategory', ProductCategorySchema)
