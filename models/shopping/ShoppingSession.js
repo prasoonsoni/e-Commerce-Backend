1d71ebc3344be8773c29e0c8c3687e3946784573
prasoonsoni <prasoonsoni03@gmail.com>
2022-07-26 10:29:13 +0530

    Added ShoppingSession Model

---
+++ models/shopping/ShoppingSession.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const ShoppingSessionSchema = new Schema({
+    user_id: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+    total:{type:Number, required:true, default:0},
+    created_at: { type: Number, default: Date.now() },
+    modified_at: { type: Number, default: Date.now() },
+})
+
+module.exports = mongoose.model('ShoppingSession', ShoppingSessionSchema)
