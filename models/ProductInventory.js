60cd9d446f6ab409f47b7d541e24ef0f9299d592
prasoonsoni <prasoonsoni03@gmail.com>
2022-07-26 09:47:58 +0530

    Added ProductInventory Model

---
+++ models/ProductInventory.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const ProductInventorySchema = new Schema({
+    quantity:{type:Number, required:true},
+    created_at: { type: Number, default: Date.now() },
+    modified_at: { type: Number, default: Date.now() },
+    deleted_at:{type:Number}
+})
+
+module.exports = mongoose.model('ProductInventory', ProductInventorySchema)
