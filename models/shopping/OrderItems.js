47d2f8e4b05e89d07ff76cfa068ae709dbf3b28e
prasoonsoni <prasoonsoni03@gmail.com>
2022-07-27 22:42:21 +0530

    Added OrderItems Model

---
+++ models/shopping/OrderItems.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const OrderItemsSchema = new Schema({
+    order_id:{type: Schema.Types.ObjectId, ref: 'OrderDetails'},
+    product_id:{type: Schema.Types.ObjectId, ref: 'Product'},
+    quantity:{type:Number, default:1, required:true},
+    created_at:{type:Number, default:Date.now()},
+    modified_at:{type:Number, default:Date.now()}
+})
+
+module.exports = mongoose.model('OrderItems', OrderItemsSchema)
