728ea6b387aa55ad2846dfbd09ee7786f2b073d2
prasoonsoni <prasoonsoni03@gmail.com>
2022-07-29 23:17:23 +0530

    Added router for user login

--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -28,4 +28,23 @@ const addUser = async (req, res) => {
     }
 }
 
-module.exports = { addUser }
+const loginUser = async (req, res) => {
+    try {
+        const { username, password } = req.body
+        const user = await User.findOne({ username })
+        if (!user) {
+            return res.json({ success: false, message: 'User not found' })
+        }
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) {
+            return res.json({ success: false, message: 'Incorrect password' })
+        }
+        const token = jwt.sign({ id: user._id }, 'secret', { expiresIn: '1h' })
+        return res.json({ success: true, message: 'User logged in successfully', token })
+    } catch (error) {
+        console.log(error.message)
+        res.json({ success: false, message: "Some Internal Server Error Occured." })
+    }
+}
+
+module.exports = { addUser, loginUser }
--- routes/user/userRoutes.js
+++ routes/user/userRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const userController = require('../../controllers/user/userController')
 
 router.post('/', userController.addUser)
+router.get('/', userController.loginUser)
 
 module.exports = router
